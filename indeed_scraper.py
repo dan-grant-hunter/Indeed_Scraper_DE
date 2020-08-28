d5b13b84035366688aef8bcb76ceb12612d9bb42
Dan Grant-Hunter <dan.granthunter@gmail.com>
2020-08-28 02:18:51 +0200

    Job links prepared and are now full URL links

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -6,23 +6,23 @@ NUMBER_OF_SEARCH_PAGES = 10
 PAGE_RESULTS_NUMBERS = list(range(0, NUMBER_OF_SEARCH_PAGES*11, 10))
 
 # Loop through number of search pages
-for search_page in PAGE_RESULTS_NUMBERS:
-    # Set up links for scraping
-    url = f'https://de.indeed.com/Jobs?l=Berlin&sort=date&lang=en&start={search_page}'
+def main_page_setup():
+    # for search_page in PAGE_RESULTS_NUMBERS:
+    #     # Set up links for scraping
+    url = f'https://de.indeed.com/Jobs?l=Berlin&sort=date&lang=en&start=0'
     main_jobs_page = requests.get(url).text
     soup = BeautifulSoup(main_jobs_page, 'lxml')
 
-    # Find all elements on page
+    # Prepare list of job links
     job_links = []
     results = soup.find_all('div', {"data-tn-component":"organicJob"})
     for i in range(len(results)):
-        job_link = results[i].h2.a['href']
+        job_link = f"{URL_PREFIX}{results[i].h2.a['href']}"
         job_links.append(job_link)
 
-# print(len(job_links))
-# for link in job_links:
-#     print(f'{URL_PREFIX}{link}')
 
+
+main_page_setup()
 # Loop through elements and open
 
 
